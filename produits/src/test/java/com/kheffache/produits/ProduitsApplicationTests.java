1a9ce6155715022bb43d0d701e0c11088d10e2c5
cherif <kheffacherif@yahoo.fr>
2023-01-17 16:17:36 +0100

    fist commit

---
+++ produits/src/test/java/com/kheffache/produits/ProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kheffache.produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
